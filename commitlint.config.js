f0ec2148a98a808e65662792d79308d4290b2922
mark <fulguerinasmarkjason0311@gmail.com>
2026-05-22 16:02:59 +0800

    chore: remove unnecessary husky.sh sourcing in commit-msg and ensure body-max-line-length rule is enforced in commitlint configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "header-max-length": [2, "always", 170],
+    "body-max-line-length": [2, "always", 170],
   },
 };
